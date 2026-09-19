41baff16df0d7d7d10a63fcfb10486f704d4bc86
longqijian <longqijian@meipinglanhui.com>
2026-09-19 21:58:42 +0800

    优化tab效果

--- MaiziWPF/Views/MainView.xaml.cs
+++ MaiziWPF/Views/MainView.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Media;
+using MaiziWPF.Core;
 using MaiziWPF.ViewModels;
 
 namespace MaiziWPF.Views
@@ -70,6 +74,63 @@ namespace MaiziWPF.Views
             if (!_isMouseInPopup && DataContext is MainViewModel vm)
             {
                 vm.IsMoreMenuOpen = false;
+            }
+        }
+
+        private void TabRegionControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (sender is not TabControl tabControl) return;
+
+            tabControl.Dispatcher.BeginInvoke(new System.Action(() =>
+            {
+                var template = tabControl.Template;
+                if (template == null) return;
+
+                var scrollViewer = template.FindName("PART_HeaderScrollViewer", tabControl) as ScrollViewer;
+                var leftButton = template.FindName("PART_ScrollLeft", tabControl) as RepeatButton;
+                var rightButton = template.FindName("PART_ScrollRight", tabControl) as RepeatButton;
+
+                if (scrollViewer == null || leftButton == null || rightButton == null) return;
+
+                const double scrollOffset = 120;
+
+                leftButton.Click += (_, _) =>
+                {
+                    scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset - scrollOffset);
+                };
+
+                rightButton.Click += (_, _) =>
+                {
+                    scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset + scrollOffset);
+                };
+
+                scrollViewer.ScrollChanged += (_, _) =>
+                {
+                    leftButton.Visibility = scrollViewer.HorizontalOffset > 0
+                        ? System.Windows.Visibility.Visible
+                        : System.Windows.Visibility.Collapsed;
+                    rightButton.Visibility = scrollViewer.HorizontalOffset < scrollViewer.ScrollableWidth
+                        ? System.Windows.Visibility.Visible
+                        : System.Windows.Visibility.Collapsed;
+                };
+
+                leftButton.Visibility = System.Windows.Visibility.Collapsed;
+
+                // 设置首页 Tab 的 MinWidth
+                ApplyDashboardTabMinWidth(tabControl);
+            }), System.Windows.Threading.DispatcherPriority.Loaded);
+        }
+
+        private void ApplyDashboardTabMinWidth(TabControl tabControl)
+        {
+            for (int i = 0; i < tabControl.Items.Count; i++)
+            {
+                if (tabControl.ItemContainerGenerator.ContainerFromIndex(i) is TabItem tabItem
+                    && tabItem.Content is FrameworkElement view
+                    && (view.DataContext as ITabItemInfo)?.Component == "DashboardView")
+                {
+                    tabItem.MinWidth = 110;
+                }
             }
         }
     }
